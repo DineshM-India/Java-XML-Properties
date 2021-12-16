a6aa4009b70f93613daa24635cd4ce9dff492896
Dinesh M <dineshtechdeveloper@gmail.com>
2021-12-16 18:55:13 +0530

    ResourceBundle

--- JavaWorkouts/src/main/java/resourceBundle/ResourceBundle.java
+++ JavaWorkouts/src/main/java/resourceBundle/ResourceBundle.java
@@ -1,5 +1,17 @@
 package resourceBundle;
 
-public class ResourceBundle {
+//import java.io.FileReader;
+//import java.util.Properties;
+import java.util.ResourceBundle;
 
-}
+public class ResourceBundle {  
+public static void main(String[] args)throws Exception{  
+/* FileReader reader=new FileReader("mobile.properties");  
+   Properties obj=new Properties();  
+   obj.load(reader);*/
+    ResourceBundle rb = ResourceBundle.getBundle("mobile");
+    System.out.println(rb.getString("price"));  
+    System.out.println(rb.getString("name"));
+    System.out.println(rb.getString("model"));
+}  
+}  
